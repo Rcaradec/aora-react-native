e01f265a0cdaf554529dc27932fd030cc586c8f5
rcaradec <rcaradec.dev@gmail.com>
2024-11-16 00:19:07 +0100

    Add NativeWind V4

--- app/index.jsx
+++ app/index.jsx
@@ -1,11 +1,11 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { Text, View } from "react-native";
 import { Link } from "expo-router";
 
 const RootLayout = () => {
   return (
-    <View style={styles.container}>
-      <Text>Aora!</Text>
+    <View className='flex-1 items-center justify-center bg-red'>
+      <Text className='text-5xl'>Aora!</Text>
       <StatusBar style='auto' />
       <Link href='/profile' style={{ color: "#c5c5ef" }}>
         Go to Profile
@@ -15,13 +15,3 @@ const RootLayout = () => {
 };
 
 export default RootLayout;
-
-const styles = StyleSheet.create({
-  container: {
-    display: "flex",
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
